09a03b202cf788e8e0fadf8d34c23b6151c3f454
Leonid Podriz <leonidpodriz@pm.me>
2024-01-18 18:48:27 +0200

    create unauthenticated requests tests for applications

--- applications/tests.py
+++ applications/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from rest_framework import status, routers
+from rest_framework.test import APIRequestFactory
+from applications.views import ApplicationViewSet
 
-# Create your tests here.
+APPLICATIONS_V1_URI = '/api/v1/applications/'
+
+
+def test_unauthorized_requests():
+    factory = APIRequestFactory()
+    view = ApplicationViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    })
+
+    requests = [
+        factory.get(APPLICATIONS_V1_URI),
+        factory.put(APPLICATIONS_V1_URI),
+        factory.patch(APPLICATIONS_V1_URI),
+        factory.delete(APPLICATIONS_V1_URI)
+    ]
+
+    for request, args in requests:
+        response = view(request)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED, (
+            'Expected Response Code 401, received {0} instead. (Method: {1})'.format(
+                response.status_code,
+                request.method
+            )
+        )
