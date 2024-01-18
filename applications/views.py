ce8ce6f0266dcfeca5dbda9fb37aa96e8d51a750
Leonid Podriz <leonidpodriz@pm.me>
2024-01-18 19:13:52 +0200

    implement application view set along with base model and serializer

--- applications/views.py
+++ applications/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from applications.models import Application
+from applications.serializers import ApplicationSerializer
+
+
+class ApplicationViewSet(viewsets.ModelViewSet):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
+    permission_classes = (permissions.IsAuthenticated,)
